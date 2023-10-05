1ede79bc9eebe15e83e20183774a0c0bb2a25e21
Laurent Seguin <4620938+NightFaust@users.noreply.github.com>
2023-10-05 10:01:48 +0200

    image, title, etc

--- src/server/api/routers/posts.ts
+++ src/server/api/routers/posts.ts
@@ -1,10 +1,51 @@
+import { auth, clerkClient } from "@clerk/nextjs";
 import { z } from "zod";
+import type { User } from "@clerk/nextjs/api";
 
 import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
+import { TRPCError } from "@trpc/server";
+
+const filerUserForClient = (user: User) => {
+    return {
+        id: user.id,
+        username: user.username,
+        profilePicture: user.imageUrl,
+    };
+}
 
 export const postRouter = createTRPCRouter({
-    getAll: publicProcedure.query(({ ctx }) => {
-        return ctx.db.post.findMany();
+    getAll: publicProcedure.query(async ({ ctx }) => {
+        const posts = ctx.db.post.findMany({
+            take: 100,
+        });
+
+        const users = (
+            await clerkClient.users.getUserList({
+                userId: (await posts).map((post) => post.authorId),
+                limit: 100,
+            })
+        ).map(filerUserForClient);
+
+        console.log('Users:' + users);
+
+        return (await posts).map(post => {
+            const author = users.find((user) => user.id === post.authorId);
+
+            if (!author || !author.username) {
+                throw new TRPCError({
+                    code: "INTERNAL_SERVER_ERROR",
+                    message: "Author for post not fount",
+                });
+            }
+
+            return {
+                post,
+                author: {
+                    ...author,
+                    username: author.username,
+                },
+            };
+        });
     }),
 });
 
