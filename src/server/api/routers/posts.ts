0c8ee0b8c6a3e58be3bdf9aef1a135730d502422
Laurent Seguin <4620938+NightFaust@users.noreply.github.com>
2023-10-06 21:12:35 +0200

    added rate limiter upstash for create post

--- src/server/api/routers/posts.ts
+++ src/server/api/routers/posts.ts
@@ -4,6 +4,8 @@ import type { User } from "@clerk/nextjs/api";
 import { createTRPCRouter, privateProcedure, publicProcedure } from "~/server/api/trpc";
 import { TRPCError } from "@trpc/server";
 import { z } from "zod";
+import { Redis } from "@upstash/redis/nodejs";
+import { Ratelimit } from "@upstash/ratelimit";
 
 const filerUserForClient = (user: User) => {
     return {
@@ -12,6 +14,13 @@ const filerUserForClient = (user: User) => {
         profilePicture: user.imageUrl,
     };
 }
+
+const ratelimit = new Ratelimit({
+    redis: Redis.fromEnv(),
+    limiter: Ratelimit.slidingWindow(3, "1 m"),
+    analytics: true,
+    prefix: "@upstash/ratelimit",
+});
 
 export const postRouter = createTRPCRouter({
     getAll: publicProcedure.query(async ({ ctx }) => {
@@ -55,6 +64,14 @@ export const postRouter = createTRPCRouter({
         }))
         .mutation(async ({ ctx, input }) => {
             const authorId = ctx.userId;
+
+            const { success } = await ratelimit.limit(authorId);
+
+            if (!success) {
+                throw new TRPCError({
+                    code: "TOO_MANY_REQUESTS",
+                });
+            }
 
             const post = await ctx.db.post.create({
                 data: {
